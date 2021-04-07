75084263d21e2b86c0653fc606c7bf69b3c3efa3
Hartman Tam <hartmantam@pm.me>
2021-04-07 09:19:38 -0700

    hotfix: pretend handling error

--- webview.js
+++ webview.js
@@ -1,8 +1,9 @@
 module.exports = (Franz) => {
     const getMessages = function getMessages() {
-        const unread = document.querySelector("#app > div > div > div.AppSidebarLayout > div > div > div > div.AppSidebarLayout-sidebar > div.NavigationList > nav > section:nth-child(2) > ul > li > a > div > div").innerText;
-
-        Franz.setBadge(unread)
+        try {
+            const unread = document.querySelector("#app > div > div > div.AppSidebarLayout > div > div > div > div.AppSidebarLayout-sidebar > div.NavigationList > nav > section:nth-child(2) > ul > li > a > div > div").innerText;
+            Franz.setBadge(unread)
+        } catch (err) {}
     };
 
     Franz.loop(getMessages);
